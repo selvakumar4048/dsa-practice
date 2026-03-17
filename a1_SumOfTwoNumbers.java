18a1b0044d1948da09aa0c1ce28a460d9a419cc7
selvakumar4048 <Iamselvakumar48@gmail.com>
2026-03-17 14:29:49 +0530

    Add files via upload

---
+++ a1_SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+
+public class a1_SumOfTwoNumbers {
+    
+    public int sum(int a, int b){
+        return a+b;
+    }
+    
+    public static void main(String[] args) {
+        a1_SumOfTwoNumbers ans=new a1_SumOfTwoNumbers();
+        int x=ans.sum(10,20);
+        System.out.println(x);
+    }
+    
+}
